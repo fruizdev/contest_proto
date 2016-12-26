574d323ef7ff7d4fb42bcb465d846c051f77b715
FranciscoRuiz <franruiz21@gmail.com>
2016-12-25 22:31:27 -0300

    validacion inscripciones, avanvces generacion de llaves, con angular directive

--- js/controllers.js
+++ js/controllers.js
@@ -1,23 +1,101 @@
- 
 var app = angular.module('TournamentGen', []);
 
-
-/**
-app.controller('customersCtrl', function($scope, $http) {
-    $http.get("customers.gfphp").then(function (response) {
-        $scope.names = response.data.records;
-    });
-});
- */
-
-app.controller('Insc_Ctrl', ['$scope', function ($scope) {
+app.controller('Insc_Ctrl2', ['$scope', function ($scope) {
     // Define $scope.competitor as an array
-    $scope.competitor = [];
+    $scope.contester = [];
     // Create a counter to keep track of the additional competitor inputs
-    $scope.inputCounter = 0;
+    $scope.contesterCounter = 0;
     // This is just so you can see the array values changing and working! Check your console as you're typing in the inputs :)
-    $scope.$watch('competitor', function (value) {         console.log(value);     }, true);
+    $scope.$watch('contester', function (value) {         console.log(value);     }, true);
 }]);
+
+
+// I've created this directive as an example of $compile in action. 
+app.directive('addToTheList', ['$compile', function ($compile) { // inject $compile service as dependency
+    return {
+        restrict: 'A',
+        link: function (scope, element, attrs,$scope) {
+         // click on the button to add new iteme in the list
+            var btnAdd =  angular.element(document.querySelector( '#addComp' ));
+            var btnGen =  angular.element(document.querySelector( '#genkeys' ));
+            var btnRestart =  angular.element(document.querySelector( '#restartTournament' ));
+            var labelGen =  angular.element(document.querySelector( '#genResult' ));
+            var contesterInput =  angular.element(document.querySelector( '#contesterInput' ));
+            var ELIM =     angular.element(document.querySelector( '#elim' ));
+            var count;
+            
+          
+            btnAdd.bind('click', function () { 
+            if( scope.contester.name.length >1 ){
+                count = scope.contester.push(scope.contester.name);
+                console.log( count + ") .- " + scope.contester.name);
+                var listItem = angular.element('<li class="mdl-list__item"> <span class="mdl-list__item-primary-content">'+count+') ' + scope.contester.name + ' </span></li>');
+                // Compile the HTML and assign to scope
+               // var compile = $compile(listItem)(scope);
+                // add competitors to the list
+                element.find('ol').append(listItem);
+                //clean competitors input  
+                contesterInput.prop('value','') ;
+                scope.contester.name=null;
+                // Increment the counter for the next input to be added
+                scope.contesterCounter++;
+            }
+            });
+              
+            btnRestart.bind('click', function () {
+                labelGen.empty();
+                contesterInput.prop('value','') ;
+                element.find('ol').empty();
+                scope.contester =[];
+                         
+        });
+            
+           // GENERAR LLAVES  !!!!!!!!!!!!!!!!!
+            btnGen.bind('click', function () {
+            if( count >=4){
+            labelGen.empty();
+            for (exp = 1; count >=  Math.pow(2, exp) ; exp++) {
+                var cupos_1ronda= Math.pow(2, exp) ;
+                var cupos_elim = count - Math.pow(2, exp) ;  
+                var participantes_1ronda =  Math.pow(2, exp) - ( count  - Math.pow(2, exp) );           
+	        }  
+                
+            var genResult = angular.element('<h4> <strong>' + count  + '</strong> Participantes.<br> Hay <strong>' + cupos_1ronda + '</strong> cupos en primera ronda,<strong> '  + cupos_elim +'</strong> a definir desde eliminatorias.</h4>');
+            labelGen.append(genResult); 
+            
+             // definir vs de eliminatoria
+             // empieza en cupos_1ronda + 1, hasta participantes	total
+            for( i = (participantes_1ronda+1); i <= count; i=i+2 ){
+               
+             var round = angular.element( '<p>HHH</p>' );// '<li class="mdl-list__item>"' + scope.contester[i] +' v/s ' + scope.contester[i+1]  + '</li>') ;   
+               
+              ELIM.append(round);
+              console.log( scope.contester[i] + " v/s " + scope.contester[i+1] );                        
+            }
+                
+                       
+              }                
+            });     
+        }
+    }
+}]);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 // I've created this directive as an example of $compile in action. 
 app.directive('addInput', ['$compile', function ($compile) { // inject $compile service as dependency
